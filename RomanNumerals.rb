c1a2d5179ec3a27d8de6cbd5dab6562789488b1e
Timothy Nordloh <tnordloh@gmail.com>
2014-08-29 16:09:54 -0600

    passing tests, almost have roman numeral tests done

--- RomanNumerals.rb
+++ RomanNumerals.rb
@@ -2,21 +2,62 @@
 
 class RomanNumerals
   def initialize 
-    @arabic_roman_map = { 1 =>    "I", 4 => "IV",
-                        5 =>    "V", 9 => "IX",
-                        10 =>   "X", 40 => "XL",
-                        50 =>   "L", 90 => "XC",
-                        100 =>  "C", 400 => "CD",
-                        500 =>  "D", 900 => "CM",
-                        1000 => "M"
+    @arabic_roman_map = { 1 => "I",   4 => "IV",
+                          5 => "V",   9 => "IX",
+                         10 => "X",  40 => "XL",
+                         50 => "L",  90 => "XC",
+                        100 => "C", 400 => "CD",
+                        500 => "D", 900 => "CM",
+                       1000 => "M"
                       }
   end
-  def is_roman?
+  def is_arabic? number
+    my_number = number.to_i 
+    if my_number != 0 && (1..3999).include?(my_number)
+      return true 
+    end
+    return false
+  end
+  def arabic_each roman_numeral
+    working_number = roman_numeral.dup
+    reversed_map = @arabic_roman_map.invert
+    #arabic_each working_number {|numeral| value += reversed_map[numeral]   }
+    while working_number.size > 0
+      if working_number.size >= 2 && reversed_map[working_number[-2,2]]
+        yield reversed_map[working_number[-2,2]]
+        working_number.chop!.chop!
+      elsif reversed_map[working_number[-1]]
+        yield reversed_map[working_number[-1]]
+        working_number.chop!
+      end
+    end
+  end
+  def is_roman? roman_number
+    remainder= roman_number.dup
+    @arabic_roman_map.each_value {|value|
+      remainder.delete!(value)
+    }
+    if remainder.size > 0 
+      return false 
+    end
+    return true
+  end
+  def roman_digits_in_order? roman_number
+    map_reverse = @arabic_roman_map.invert
+    last_value = 0
+    arabic_each(roman_number) {|numeral| 
+      if last_value <= numeral
+        last_value = numeral
+      else
+       false 
+      end
+    }
+    true
   end
   def to_roman arabic_number
     value = ""
-    return nil if arabic_number > 3999
-    return nil if arabic_number < 0
+    arabic_number = arabic_number.to_i
+    return nil unless is_arabic? arabic_number
     while arabic_number > 0
       arabic_roman_map_cur = @arabic_roman_map.select {|k,v| k <= arabic_number}
       highest_value = arabic_roman_map_cur.keys.max
@@ -27,19 +68,18 @@ class RomanNumerals
     value
   end
   def to_arabic roman_number
-    working_number = roman_number.dup
+    return nil unless is_roman? roman_number
+    # roman_digits_in_order? arabic_number
     value = 0
-    reversed_map = @arabic_roman_map.invert
-    arabic_each working_number {|numeral| value += reversed_map[numeral]   }
-    while working_number.size > 0
-      if working_number.size >= 2 && reversed_map[working_number[-2,2]]
-        value += reversed_map[working_number[-2,2]]
-        working_number.chop!.chop!
-      elsif reversed_map[working_number[-1]]
-        value += reversed_map[working_number[-1]]
-        working_number.chop!
-      end
-    end
+    arabic_each(roman_number) {|numeral| value += numeral   }
     return value
+  end
+  def convert value
+    return to_arabic(value) if is_roman?(value)
+    return  to_roman(value) if is_arabic?(value)
+    "the entry '#{value}' doesn't look roman or arabic.\n"+
+    "Please either enter a roman numeral " +
+    "(containing "  + @arabic_roman_map.values.join(',') + ")" +
+    "\nor an arabic number between 1 and 3999"
   end
 end
--- test/TestRomanNumerals.rb
+++ test/TestRomanNumerals.rb
@@ -7,8 +7,7 @@ class TestRomanNumerals < MiniTest::Unit::TestCase
   def setup
     @rn = RomanNumerals.new
   end
-
- def test_all_to_roman_possibilities
+  def test_all_to_roman_possibilities
     list = []
     File.open('known_numerals').each { |line|
       line.chomp!
@@ -18,9 +17,9 @@ class TestRomanNumerals < MiniTest::Unit::TestCase
     1.upto(list.size-1) {|i|
       assert_equal list[i], @rn.to_roman(i) 
     }
- end
+  end
 
- def test_all_to_arabic_possibilities
+  def test_all_to_arabic_possibilities
     list = []
     File.open('known_numerals').each { |line|
       line.chomp!
@@ -30,9 +29,13 @@ class TestRomanNumerals < MiniTest::Unit::TestCase
     (1...(list.size)).each {|i|
       assert_equal i, @rn.to_arabic(list[i]) 
     }
- end
+  end
 
- def test_that_will_be_skipped
-   skip "test this later"
- end
+  def test_that_non_arabic_numbers_return_nil
+    assert_equal nil, @rn.to_arabic("hello") 
+  end
+
+  def test_that_non_roman_numbers_return_nil
+    assert_equal nil, @rn.to_roman("hello") 
+  end
 end
